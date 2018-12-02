7ea77796131fcaf689e132b74dd0e45dcb799aa4
centurionservise <centurionservise@rambler.ru>
2018-12-02 23:29:38 +0200

    ccc

--- PrivatBank.py
+++ PrivatBank.py
@@ -32,26 +32,17 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
         self.DB='privat_api.db'
         self.privat_txt='privat_api.txt'
         self.url='https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
-        # cursor=None
-        # connector=None
         self.line_empty='----------------------------------------------'
         self.line_header='''----------------------------------------------
             Privat Bank - API
 ----------------------------------------------'''
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+
         # self.setWindowTitle("Hello world")
         self.connector, self.cursor=self.db_connect()
         self.lcdNumber.display(self.records_count())
-        # self.lcdNumber.
-
-        # self.horizontalSlider_sel_record.setMinimum(1)
-        # self.horizontalSlider_sel_record.setMaximum(self.records_count())
-        # self.horizontalSlider_sel_record.setValue(1)
-        # self.lcdNumber_sel_record.display(self.horizontalSlider_sel_record.value())
-
         self.horizontalSlider_sel_record.valueChanged.connect(self.select_record)
-        # self.btn_request.clicked.connect(self.main_func(self.get_req(self.url),self.cursor, self.connector,self.privat_txt))
         self.btn_request.clicked.connect(self.main_func)
         self.btn_exit.clicked.connect(self.close)
         self.btn_load.clicked.connect(self.get_record)
@@ -63,22 +54,12 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
 
         self.first_init()
         
-        # self.label_period_end.text(self.period[1])
- 
-    
-    # def CopyText(self):
-    #     self.textEdit_1.append(self.lineEdit_1.text())
-    #     self.lineEdit_1.setText("SERG")
-    #     # window.setWindowTitle("PyQt")
-    #     pass
-    # def
-
 
     def first_init(self):
+        self.api_status()
         self.set_period()
 
         if self.records_count()==0:
-            # self.lcdNumber_sel_record.display("--")
             self.horizontalSlider_sel_record.setMinimum(0)
             self.horizontalSlider_sel_record.setMaximum(0)
             self.horizontalSlider_sel_record.setValue(0)
@@ -104,13 +85,11 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
         win32api.ShellExecute(0,'printto',filename,'"%s"' % win32print.GetDefaultPrinter(),'.',0)
 
     def print_result(self):
-        # print(self.textEdit.toPlainText())
         text_from_textEdit=self.textEdit.toPlainText()
         if text_from_textEdit:
             self.print_text(text_from_textEdit)
 
     def copy_text(self):
-        # self.textEdit.selectAll()
         self.textEdit.copy()
         
     def move_left(self):
@@ -121,13 +100,11 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
         '''Function return exect records from DB'''
         date=self.get_date()
         try:
-            # record_number=self.horizontalSlider_sel_record.value()
             self.cursor.execute("SELECT * FROM Exchange_Rates WHERE date='{}'".format(date))
             row=self.cursor.fetchall()
 
             if row:
                 self.textEdit.clear()
-                # counter_big=1
                 counter_small=1
                 self.textEdit.append('Record '+str(row[0][0])+':')
                 self.label_date.setText(date)
@@ -135,7 +112,6 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
                 for record in row:
                     if counter_small==5:
                         self.textEdit.append('')
-                        # counter_big+=1
                         self.textEdit.append('Record '+str(record[0])+':')
                         counter_small=1  
                     temp_str=str(record[1])+"   "+str(record[2])+"   "+str(record[3])+"   "+str(record[4])
@@ -152,13 +128,11 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
         month=self.calendarWidget.selectedDate().month()
         day=self.calendarWidget.selectedDate().day()
         date=str(day)+'-'+str(month)+'-'+str(year)
-        # print(date)
         self.label_calendar.setText(date)
         return date
 
 
     def select_record(self):
-        # self.horizontalSlider_sel_record.setMaximum(self.records_count())
         self.lcdNumber_sel_record.display(self.horizontalSlider_sel_record.value())
 
     def records_count(self):
@@ -168,8 +142,6 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
             row=self.cursor.fetchall()
             return(len(row))
         except sqlite3.DatabaseError as DB_error:
-            # print("def records_count ->")
-            # print("Error was found with DB or table ...")
             print("sqlite3.DatabaseError: ",DB_error)
             return 0
 
@@ -179,8 +151,6 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
         try:
             self.cursor.execute("SELECT * FROM Exchange_Rates WHERE ccy='USD'")
             row=self.cursor.fetchall()
-            # print(len(row))
-            # print(row[-1][5])
             
             if len(row)==0:
                 return ("00-00-0000","00-00-0000")
@@ -188,8 +158,6 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
                 return (row[0][5],row[0][5])
             elif len(row)>1:
                 return (row[0][5],row[-1][5])
-            # else:
-            #     return (row[0][5],row[0][5])
 
         except sqlite3.DatabaseError as DB_error:
             print("sqlite3.DatabaseError: ",DB_error)
@@ -202,12 +170,13 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
             self.cursor.execute("SELECT * FROM Exchange_Rates WHERE id='{}'".format(record_number))
             row=self.cursor.fetchall()
 
-            self.textEdit.clear()
-
-            for record in row:
-                temp_str=str(record[1])+"   "+str(record[2])+"   "+str(record[3])+"   "+str(record[4])
-                self.textEdit.append(temp_str)
-                self.label_date.setText(str(record[5]))
+            # self.textEdit.clear()
+            # print(row)
+            self.fill_table_BD(row)
+            # for record in row:
+            #     temp_str=str(record[1])+"   "+str(record[2])+"   "+str(record[3])+"   "+str(record[4])
+                # self.textEdit.append(temp_str)
+                # self.label_date.setText(str(record[5]))
             
 
         except sqlite3.DatabaseError as DB_error:
@@ -226,50 +195,52 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
             print("sqlite3.DatabaseError: ", DB_error)
             return (None, None)
 
-    def get_req(self, temp_url):
+    def get_request(self, temp_url):
         '''Function gets Data by Url and returns result of request''' 
         try:
-            # print("ZZZ")
             return (requests.get(temp_url)).json()
         except :
             print("There was an error with the request")
             print("Check Internet Connection or PrivatBank API url")
             return None
 
+    def api_status(self):
+        if self.get_request(self.url)!=None:
+            self.label_PB_status.setText('GOOD')
+            self.label_PB_status.setStyleSheet('color: green')
+            return 1
+        else:
+            self.label_PB_status.setText('BAD')
+            self.label_PB_status.setStyleSheet('color: red')
+            return 0
+
     def main_func(self):
-        from_PB=self.get_req(self.url)
+        self.api_status()
+        from_PB=self.get_request(self.url)
         temp_id=self.records_count()+1
-        # print(temp_id)
         temp_list=[]
         temp_time=None
         now = datetime.datetime.now()
-        self.textEdit.clear()
-        # print(now)
+        # self.textEdit.clear()
 
         if from_PB!=None:
-            temp_time=now.strftime("%#d-%#m-%#Y %H:%M:%S")
-            self.label_date.setText(now.strftime("%#d-%#m-%#Y"))
-            # print(temp_time)
-            # print(self.line_header)
+            temp_time=now.strftime("%d-%m-%#Y %H:%M:%S")
+            # self.label_date.setText(now.strftime("%d-%m-%#Y"))
             counter=1
             self.textEdit.clear()
+            self.fill_table(from_PB,temp_id,now.strftime("%d-%m-%#Y"))
 
             for i in from_PB:
                 line_main='{}. {}: buy - {:.2f} {} / sale - {:.2f} {}'.format(counter, i['ccy'], float(i['buy']), i['base_ccy'], float(i['sale']),i['base_ccy'])
                 temp_list.append(line_main)
-                # print(line_main)
-                self.textEdit.append(line_main)
+                # self.textEdit.append(line_main)
                 counter+=1
-                # print(counter)
                 try:
-                    self.cursor.execute("INSERT INTO Exchange_Rates (id,ccy,base_ccy,buy,sale,date,time) VALUES (?,?,?,?,?,?,?)",(temp_id,i['ccy'], i['base_ccy'],i['buy'],i['sale'],now.strftime("%#d-%#m-%#Y"),now.strftime("%H:%M:%S")))
-                    # pass
-                    # print("record added")
+                    self.cursor.execute("INSERT INTO Exchange_Rates (id,ccy,base_ccy,buy,sale,date,time) VALUES (?,?,?,?,?,?,?)",(temp_id,i['ccy'], i['base_ccy'],i['buy'],i['sale'],now.strftime("%d-%m-%#Y"),now.strftime("%H:%M:%S")))
                 except sqlite3.DatabaseError as err:
                     print("Error SQLite3: ", err)
                 else:
                     self.connector.commit()
-            # print(self.line_empty)
 
             with open(self.privat_txt,'w') as file:
                 file.write(temp_time)
@@ -283,18 +254,47 @@ class SergWindow(QtWidgets.QMainWindow, Ui_Form):
             self.set_period()
         
         else:
-            # print("Chack Internet Connection or PrivatBank API url")
             pass
-        # print("Exit Function")
 
 
-    # def browse_folder(self):
-    #     self.listWidget.clear()  # На случай, если в списке уже есть элементы
-    #     directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+    def fill_table(self,requests_result,rec_number,rec_date):
+        for i in requests_result:
+            if i['ccy']=='USD':
+                self.label_usd_buy.setText('{:.2f}'.format(float(i['buy'])))
+                self.label_usd_sale.setText('{:.2f}'.format(float(i['sale'])))
+            elif i['ccy']=='EUR':
+                self.label_eur_buy.setText('{:.2f}'.format(float(i['buy'])))
+                self.label_eur_sale.setText('{:.2f}'.format(float(i['sale'])))
+            elif i['ccy']=='RUR':
+                self.label_rur_buy.setText('{:.2f}'.format(float(i['buy'])))
+                self.label_rur_sale.setText('{:.2f}'.format(float(i['sale'])))
+            elif i['ccy']=='BTC':
+                self.label_btc_buy.setText('{:.2f}'.format(float(i['buy'])))
+                self.label_btc_sale.setText('{:.2f}'.format(float(i['sale'])))
+        self.label_rec_number.setText(str(rec_number))
+        self.label_rec_date.setText(rec_date)
 
-    #     if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
-    #         for file_name in os.listdir(directory):  # для каждого файла в директории
-    #             self.listWidget.addItem(file_name)   # добавить файл в listWidget
+    def fill_table_BD(self,requests_BD):
+        # for record in row:
+                # temp_str=str(record[1])+"   "+str(record[2])+"   "+str(record[3])+"   "+str(record[4])
+        for i in requests_BD:
+            rec_number=i[0]
+            rec_date=i[5]
+            if i[1]=='USD':
+                self.label_usd_buy.setText('{:.2f}'.format(float(i[2])))
+                self.label_usd_sale.setText('{:.2f}'.format(float(i[3])))
+            elif i[1]=='EUR':
+                self.label_eur_buy.setText('{:.2f}'.format(float(i[2])))
+                self.label_eur_sale.setText('{:.2f}'.format(float(i[3])))
+            elif i[1]=='RUR':
+                self.label_rur_buy.setText('{:.2f}'.format(float(i[2])))
+                self.label_rur_sale.setText('{:.2f}'.format(float(i[3])))
+            elif i[1]=='BTC':
+                self.label_btc_buy.setText('{:.2f}'.format(float(i[2])))
+                self.label_btc_sale.setText('{:.2f}'.format(float(i[3])))
+        self.label_rec_number.setText(str(rec_number))
+        self.label_rec_date.setText(rec_date)
+    
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
